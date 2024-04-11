be0d75a856a167071df24281772b9d65eea94b27
yong <yong@asial.co.jp>
2024-04-11 15:41:55 +0900

    init

---
+++ ask.py
@@ -0,0 +1,68 @@
+from langchain_community.llms import Ollama
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+import sys
+
+ollama = Ollama(model="mistral:7b")
+
+def answer_with_context(question, context_url):
+    # Now let's load a document to ask questions against. 
+    # You might need to install `bs4`.
+    loader = WebBaseLoader(context_url)
+    data = loader.load()
+
+    # This file is pretty big. Which means the full document won't fit into the context for the model. So we need to split it up into smaller pieces.
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    all_splits = text_splitter.split_documents(data)
+
+    # It's split up, but we have to find the relevant splits and then submit those to the model. 
+    # We can do this by creating embeddings and storing them in a vector database. 
+    # We can use Ollama directly to instantiate an embedding model. 
+    # You might need to install `chromadb`.
+    oembed = OllamaEmbeddings(model="nomic-embed-text")
+    vectorstore = Chroma.from_documents(documents=all_splits, embedding=oembed)
+
+    # And here is the relevant part of the document to the question.
+    docs = vectorstore.similarity_search(question)
+    print("\n\n#### Context From Document ####")
+    print("url: ", context_url)
+    print(docs)
+
+    # The next thing is to send the question and the relevant parts of the docs to the model to see if we can get a good answer. 
+    qachain = RetrievalQA.from_chain_type(ollama, retriever=vectorstore.as_retriever())
+    output = qachain.invoke({"query": question})
+    return output['result']
+
+def answer(question):
+    output = ollama.invoke(question)
+    return output
+
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide a topic as an argument.")
+        print('Usage 1: python ask.py "What is Asial Company?"')
+        print('Usage 2: python ask.py "What is Asial Company?" "https://en.asial.co.jp/about"')
+
+    question = sys.argv[1]
+    context_url = ''
+
+    # Now let's ask a question from the document.
+    print("\n\n#### Question ####")
+    print(question)
+
+    if len(sys.argv) == 3:
+        context_url = sys.argv[2]
+
+    if context_url == '':
+        result = answer(question)
+    else:
+        result = answer_with_context(question, context_url)
+
+    print("\n\n#### Result ####")
+    print(result)
+
+if __name__ == '__main__':
+    main()
---
+++ blog.py
@@ -0,0 +1,81 @@
+from langchain_community.llms import Ollama
+from crewai import Agent, Task, Crew, Process
+import sys
+
+
+ollama_gemma = Ollama(model='gemma:7b')
+ollama_mistral = Ollama(model='mistral:7b')
+
+def write_blog(topic):
+  researcher = Agent(
+      role='Senior Research Analyst',
+      goal=f"Uncover research in {topic}",
+      backstory="""You work at a leading research company.
+      Your expertise lies in identifying emerging trends, research and discover.
+      You have a knack for dissecting complex data and presenting actionable insights.""",
+      verbose=True,
+      allow_delegation=False,
+      llm=ollama_gemma
+  )
+
+  writer = Agent(
+      role='Content Strategist',
+      goal=f"Craft compelling content on {topic}",
+      backstory="""You are a renowned Content Strategist, known for your insightful and engaging articles.
+      You transform complex concepts into compelling narratives.""",
+      verbose=True,
+      allow_delegation=False,
+      llm=ollama_mistral
+  )
+
+  # Create tasks for your agents
+  task1 = Task(
+    description=f"Conduct a comprehensive analysis of {topic}.",
+    expected_output="Full analysis report in bullet points",
+    agent=researcher
+  )
+
+  task2 = Task(
+    description=f"""Using the insights provided, develop an engaging blog
+    post that highlights the topic of {topic}.
+    Your post should be informative yet accessible, catering to any audience, easy to understand.
+    Make it sound cool, avoid complex words so it doesn't sound like AI.""",
+    expected_output="Full blog post of at least 4 paragraphs",
+    agent=writer
+  )
+
+  # Instantiate your crew with a sequential process
+  crew = Crew(
+      agents=[researcher, writer],
+      tasks=[task1, task2],
+      verbose=2, # You can set it to 1 or 2 to different logging levels
+  )
+
+  # Get your crew to work!
+  result = crew.kickoff()
+  return result
+
+def main():
+  if len(sys.argv) < 2:
+    print("Please provide a topic as an argument.")
+    print('Usage: python blog.py "AI and data science trends in 2024"')
+    sys.exit(1)
+
+  # Get the first command-line argument after the script name
+  topic = sys.argv[1]
+
+  # Now you can use 'argument' in your script
+  print("\n\n#### Topic ####")
+  print(topic)
+
+  if topic == '':
+    print("Topic is empty.")
+    sys.exit(1)
+
+  result = write_blog(topic)
+
+  print("\n\n#### Result ####")
+  print(result)
+
+if __name__ == '__main__':
+  main()
